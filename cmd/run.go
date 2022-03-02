25b07ca761008f33b958459d3cbce1e8f25c51d4
Clivern <hello@clivern.com>
2022-03-02 01:07:14 +0100

    refactor

--- cmd/run.go
+++ cmd/run.go
@@ -3,3 +3,30 @@
 // license that can be found in the LICENSE file.
 
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Run a kafka clusters locally",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+var runSingleNodeCmd = &cobra.Command{
+	Use:   "single",
+	Short: "Run a single kafka node locally",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Run single node")
+	},
+}
+
+func init() {
+	runCmd.AddCommand(runSingleNodeCmd)
+	rootCmd.AddCommand(runCmd)
+}
